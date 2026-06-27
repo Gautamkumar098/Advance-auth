997400a9ccef4a4f4aa65eb723f052243d70b420
Gautam kumar <gauta7318@gmail.com>
2026-06-27 07:50:23 +0530

    Update CORS configuration to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://advance-auth-six.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
