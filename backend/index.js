7b44b8b5105ab18cf75e178b55165bdc060ae51d
Gautam kumar <gauta7318@gmail.com>
2026-06-27 08:15:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ app.use(
   })
 );
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" })); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
